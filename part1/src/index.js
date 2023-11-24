2f46cbf1c9333e3bd9e3bedae75be244af3f912b
Luis Vázquez <tellez0011@gmail.com>
2023-11-24 09:44:06 -0600

    part1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,30 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  );
-};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const App = () => {
-
-  return [
-    <h1>Greetings</h1>,
-      <Hello name="Maya" age={26 + 10} />,
-      <Footer />
-  ]
-};
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
